ec98463b8984cd7b97ed091ff62ad0a5d0d2d9a3
yeahzhanym <yeahzhanym@gmail.com>
2023-11-23 18:23:10 +0600

    complete all 3 projects freecodecamp/akhils 3 project

--- freecodecamp/Akhil Sharma/Go Programming Tutorial – 3 Beginner Projects/README.md
+++ freecodecamp/Akhil Sharma/Go Programming Tutorial – 3 Beginner Projects/README.md
@@ -1,21 +1,34 @@
-### Improve you Go programming skills by developing three different projects.
+#### Improve you Go programming skills by developing three different projects.
 
 link: https://www.youtube.com/watch?v=k_V5VvYSlS4
 
+##### go health-checker
 
-## go health-checker
 ```bash
 # run command
-$ go run . --domain google.com 
+$ go run . --domain google.com
 # you can use any domain
 ```
+
 ![](go-healthcheck.png)
 
-## file-encryption-decryption
+##### file-encryption-decryption
+
 ```bash
 # run command
 $ go run . encrypt path/to/your/file
 $ go run . decrypt path/to/your/file
-# you can use any domain
+#
 ```
+
 ![](file-encryption-decryption.png)
+
+##### go-encryption
+
+```bash
+# run command change HELLOWORLD, works with only text
+$ go run main.go
+#
+```
+
+![](data-encryption-with-go.png)
---
+++ freecodecamp/Akhil Sharma/Go Programming Tutorial – 3 Beginner Projects/go-encryption/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+const originalLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func hashLetterFn(key int, letter string) (result string) {
+	runes := []rune(letter)
+	lastLetterKey := string(runes[len(letter)-key : len(letter)])
+	leftOverLetters := string(runes[0 : len(letter)-key])
+	return fmt.Sprintf(`%s%s`, lastLetterKey, leftOverLetters)
+}
+
+func encrypt(key int, plainText string) (result string) {
+	hashLetter := hashLetterFn(key, originalLetter)
+	var hashedString = ""
+	findOne := func(r rune) rune {
+		pos := strings.Index(originalLetter, string([]rune{r}))
+		if pos != -1 {
+			letterPosition := (pos + len(originalLetter)) % len(originalLetter)
+			hashedString = hashedString + string(hashLetter[letterPosition])
+		}
+		return r
+	}
+
+	strings.Map(findOne, plainText)
+	return hashedString
+}
+
+func decrypt(key int, encryptedText string) (result string) {
+	hashLetter := hashLetterFn(key, originalLetter)
+	var hashedString = ""
+	findOne := func(r rune) rune {
+		pos := strings.Index(hashLetter, string([]rune{r}))
+		if pos != -1 {
+			letterPosition := (pos + len(originalLetter)) % len(originalLetter)
+			hashedString = hashedString + string(originalLetter[letterPosition])
+			return r
+		}
+		return r
+	}
+
+	strings.Map(findOne, encryptedText)
+	return hashedString
+}
+
+func main() {
+	plainText := "HELLOWORLD"
+	fmt.Println("Plain Text", plainText)
+	encrypted := encrypt(5, plainText)
+	fmt.Println("Encrypted text", encrypted)
+	decrypted := decrypt(5, encrypted)
+	fmt.Println("Decrypted text", decrypted)
+}
